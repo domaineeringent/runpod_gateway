eea06ad23ecc6921bd0b8f3e8fdbf559d17deac2
domaineeringent <152757964+domaineeringent@users.noreply.github.com>
2025-01-27 07:22:46 -0800

    Update gateway.py

--- gateway.py
+++ gateway.py
@@ -250,4 +250,4 @@ async def get_status(pod_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080) 
+    uvicorn.run(app, host="0.0.0.0", port=8081) 
